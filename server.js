8ef6a2168c3f476307737ac55a58ab3c3b36f29c
Erwin Ramiro Flores Marca <ramiromarca6@gmail.com>
2022-07-27 12:05:40 -0400

    feat: prod v1.0.1

---
+++ server.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 5000;
+app.listen(port);
